3f2083417f08756dbac409bc035d19143c832a27
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2024-10-21 16:25:06 +0900

    2024.10.21
    1. 프로젝트생성
    2. 마우스포지션과 Grid위치 연동
    3. GridRectangularTransparent이름의 쉐이더만듬 그리드 위치를 나태내기위한 쉐이더
    4. 해당 쉐이더로 Shader Graphs_GridRectangularTransparent 메터리얼 생성

--- Assets/_Script/PlacementSystem.cs
+++ Assets/_Script/PlacementSystem.cs
@@ -1,23 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using Unity.VisualScripting;
+
 using UnityEngine;
 
 public class PlacementSystem : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject mouseIndicator;
+    private GameObject mouseIndicator, cellIndicator;
     [SerializeField]
     private InputManager inputManager;
     [SerializeField]
     private Grid grid;
 
-
     private void Update()
     {
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
     }
 }
